8449abe0ec5c648cb5edfeaf30235773b1b6f35f
Nikki <archiektor@gmail.com>
2020-04-28 18:20:09 +0200

    Updates

---
+++ precache-manifest.d9147ee501d5610d26d0988773438c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8c4132666f6443b10fc33d9de7c7b3",
+    "url": "/Coctail-DB/index.html"
+  },
+  {
+    "revision": "d29f331a0d448277d78b",
+    "url": "/Coctail-DB/static/css/main.bb35282f.chunk.css"
+  },
+  {
+    "revision": "9dc3171dac0d918b4770",
+    "url": "/Coctail-DB/static/js/2.ef25e2df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Coctail-DB/static/js/2.ef25e2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29f331a0d448277d78b",
+    "url": "/Coctail-DB/static/js/main.99eeeec3.chunk.js"
+  },
+  {
+    "revision": "e347f8c379ad07543d95",
+    "url": "/Coctail-DB/static/js/runtime-main.d1119326.js"
+  }
+]);
